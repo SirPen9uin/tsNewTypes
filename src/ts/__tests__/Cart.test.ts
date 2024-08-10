26a5d809e6d06ebe57e14292f390852d7f22412b
Pen9uin <sirpen9uin@gmail.com>
2024-08-10 15:35:19 +0300

    done

---
+++ src/ts/__tests__/Cart.test.ts
@@ -0,0 +1,20 @@
+import Cart from '../service/Cart';
+import Buyable from '../domain/Buyable';
+
+test('Testing the creation of a Cart class object', (): void => {
+  const result: Cart = new Cart();
+
+  expect(result).toBeInstanceOf(Cart);
+  expect(result).toEqual({ pvtItems: [] });
+});
+
+test('Testing adding items to a Cart class object and getting them', (): void => {
+  const cart: Cart = new Cart();
+  const item: Buyable = { id: 1, name: 'Test', price: 300 };
+
+  cart.add(item);
+
+  const result: Buyable[] = cart.items;
+
+  expect(result).toEqual([item]);
+});
